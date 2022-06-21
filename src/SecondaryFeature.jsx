55edcb609555601a85ef91f9b6c449616350367f
Andrés Quilodrán <andres.caro.q@gmail.com>
2022-06-21 19:07:28 -0400

    Baseline for exercises

--- src/SecondaryFeature.jsx
+++ src/SecondaryFeature.jsx
@@ -1,106 +1,55 @@
+const FEATURE_ITEMS = [
+  "SaaS Focused",
+  "Awesome Design",
+  "Ready to Use",
+  "Vanilla JS",
+  "Essential Sections",
+  "Highly Optimized",
+];
+
+function FeatureHeader() {
+  return (
+    <div className="row flex justify-center">
+      <div className="w-full md:w-9/12 lg:w-8/12 xl:w-6/12">
+        <div className="section-title text-center mb-15">
+          <h1 className="mb-6">Why Choose SaaSpal</h1>
+          <p className="text-lg">
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+            nonumy eirmod tempor invidunt ut labore et dolore
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function FeatureItem({ title }) {
+  return (
+    <div className="w-full md:w-1/2 lg:w-4/12">
+      <div className="p-8 lg:px-3">
+        <div className="content">
+          <h3 className="mb-5">SaaS Focused</h3>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function FeatureGrid() {
+  return (
+    <div className="row flex flex-wrap">
+      <FeatureItem />
+    </div>
+  );
+}
+
 export default function SecondaryFeature() {
   return (
-    <section id="why" class="feature-extended-section pt-25">
-      <div class="feature-extended-wrapper py-18 bg-theme-color bg-opacity-10">
-        <div class="container">
-          <div class="row flex justify-center">
-            <div class="w-full md:w-9/12 lg:w-8/12 xl:w-6/12">
-              <div class="section-title text-center mb-15">
-                <h1 class="mb-6">Why Choose SaaSpal</h1>
-                <p class="text-lg">
-                  Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
-                  diam nonumy eirmod tempor invidunt ut labore et dolore
-                </p>
-              </div>
-            </div>
-          </div>
-
-          <div class="row flex flex-wrap">
-            <div class="w-full md:w-1/2 lg:w-4/12">
-              <div class="p-8 lg:px-3">
-                <div class="feature-icon-2 mb-8 text-theme-color leading-none">
-                  <i class="lni lni-display"></i>
-                </div>
-                <div class="content">
-                  <h3 class="mb-5">SaaS Focused</h3>
-                  <p class="text-lg">
-                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
-                    diam nonumy eirmod tempor invidunt ut labore
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div class="w-full md:w-1/2 lg:w-4/12">
-              <div class="p-8 lg:px-3">
-                <div class="feature-icon-2 mb-8 text-theme-color leading-none">
-                  <i class="lni lni-leaf"></i>
-                </div>
-                <div class="content">
-                  <h3 class="mb-5">Awesome Design</h3>
-                  <p class="text-lg">
-                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
-                    diam nonumy eirmod tempor invidunt ut labore
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div class="w-full md:w-1/2 lg:w-4/12">
-              <div class="p-8 lg:px-3">
-                <div class="feature-icon-2 mb-8 text-theme-color leading-none">
-                  <i class="lni lni-grid-alt"></i>
-                </div>
-                <div class="content">
-                  <h3 class="mb-5">Ready to Use</h3>
-                  <p class="text-lg">
-                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
-                    diam nonumy eirmod tempor invidunt ut labore
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div class="w-full md:w-1/2 lg:w-4/12">
-              <div class="p-8 lg:px-3">
-                <div class="feature-icon-2 mb-8 text-theme-color leading-none">
-                  <i class="lni lni-javascript"></i>
-                </div>
-                <div class="content">
-                  <h3 class="mb-5">Vanilla JS</h3>
-                  <p class="text-lg">
-                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
-                    diam nonumy eirmod tempor invidunt ut labore
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div class="w-full md:w-1/2 lg:w-4/12">
-              <div class="p-8 lg:px-3">
-                <div class="feature-icon-2 mb-8 text-theme-color leading-none">
-                  <i class="lni lni-layers"></i>
-                </div>
-                <div class="content">
-                  <h3 class="mb-5">Essential Sections</h3>
-                  <p class="text-lg">
-                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
-                    diam nonumy eirmod tempor invidunt ut labore
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div class="w-full md:w-1/2 lg:w-4/12">
-              <div class="p-8 lg:px-3">
-                <div class="feature-icon-2 mb-8 text-theme-color leading-none">
-                  <i class="lni lni-rocket"></i>
-                </div>
-                <div class="content">
-                  <h3 class="mb-5">Highly Optimized</h3>
-                  <p class="text-lg">
-                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
-                    diam nonumy eirmod tempor invidunt ut labore
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
+    <section id="why" className="feature-extended-section pt-25">
+      <div className="feature-extended-wrapper py-18 bg-theme-color bg-opacity-10">
+        <div className="container">
+          <FeatureHeader />
+          <FeatureGrid />
         </div>
       </div>
     </section>
