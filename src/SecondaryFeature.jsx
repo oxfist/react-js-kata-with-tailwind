554af658e66c7a51009226a5cd30c4096578cc6e
Andrés Quilodrán <andres.caro.q@gmail.com>
2022-06-22 21:02:37 -0400

    Agrega instrucciones para el resto de ejercicios

--- src/SecondaryFeature.jsx
+++ src/SecondaryFeature.jsx
@@ -1,3 +1,4 @@
+/* 1. Mostrar todos los títulos */
 const FEATURE_ITEMS = [
   "SaaS Focused",
   "Awesome Design",
@@ -7,6 +8,7 @@ const FEATURE_ITEMS = [
   "Highly Optimized",
 ];
 
+/* 2. Mostrar todos los títulos habilitados */
 const FEATURE_ITEMS_ALL = [
   { title: "SaaS Focused", enabled: true },
   { title: "Time Travel", enabled: false },
@@ -20,6 +22,76 @@ const FEATURE_ITEMS_ALL = [
   { title: "Cryptocurrencies", enabled: false },
   { title: "Highly Optimized", enabled: true },
 ];
+
+/* 3. Mostrar todos los títulos habilitados ordenados por votos de mayor a menor */
+const FEATURE_ITEMS_ALL_WITH_UPVOTES = [
+  { title: "SaaS Focused", enabled: true, upvotes: 420 },
+  { title: "Time Travel", enabled: false, upvotes: 500 },
+  { title: "Awesome Design", enabled: true, upvotes: 317 },
+  { title: "Ugly Colors", enabled: false, upvotes: 189 },
+  { title: "Ready to Use", enabled: true, upvotes: 273 },
+  { title: "Unreadable Code", enabled: false, uptoves: 362 },
+  { title: "Boring Memes", enabled: false, upvotes: 240 },
+  { title: "Vanilla JS", enabled: true, uptoves: 191 },
+  { title: "Essential Sections", enabled: true, upvotes: 46 },
+  { title: "Cryptocurrencies", enabled: false, upvotes: 201 },
+  { title: "Highly Optimized", enabled: true, upvotes: 378 },
+];
+
+/* 4. Mostrar todos los títulos habilitados ordenados por votos de mayor a menor con sus respectivos logos */
+const FEATURE_ITEMS_ALL_WITH_UPVOTES_AND_LOGOS = [
+  { title: "SaaS Focused", enabled: true, upvotes: 420, logo: "lni-display" },
+  { title: "Time Travel", enabled: false, upvotes: 500, logo: "lni-timer" },
+  { title: "Awesome Design", enabled: true, upvotes: 317, logo: "lni-leaf" },
+  { title: "Ugly Colors", enabled: false, upvotes: 189, logo: "lni-close" },
+  { title: "Ready to Use", enabled: true, upvotes: 273, logo: "lni-grid-alt" },
+  {
+    title: "Unreadable Code",
+    enabled: false,
+    uptoves: 362,
+    logo: "lni-help",
+  },
+  {
+    title: "Boring Memes",
+    enabled: false,
+    upvotes: 240,
+    logo: "lni-emoji-speechless",
+  },
+  { title: "Vanilla JS", enabled: true, uptoves: 191, logo: "lni-javascript" },
+  {
+    title: "Essential Sections",
+    enabled: true,
+    upvotes: 46,
+    logo: "lni-layers",
+  },
+  { title: "Cryptocurrencies", enabled: false, upvotes: 201 },
+  {
+    title: "Highly Optimized",
+    enabled: true,
+    upvotes: 378,
+    logo: "lni-rocket",
+  },
+];
+
+/* Usar este componente para el ejercicio 4 */
+function FeatureItemWithLogo({ title }) {
+  return (
+    <div class="w-full md:w-1/2 lg:w-4/12">
+      <div class="p-8 lg:px-3">
+        <div class="feature-icon-2 mb-8 text-theme-color leading-none">
+          <i class="lni lni-leaf"></i>
+        </div>
+        <div class="content">
+          <h3 class="mb-5">{title}</h3>
+          <p class="text-lg">
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+            nonumy eirmod tempor invidunt ut labore
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 function FeatureHeader() {
   return (
@@ -38,7 +110,6 @@ function FeatureHeader() {
 }
 
 function FeatureItem({ title }) {
-  
   return (
     <div className="w-full md:w-1/2 lg:w-4/12">
       <div className="p-8 lg:px-3">
@@ -50,13 +121,15 @@ function FeatureItem({ title }) {
   );
 }
 
-const getEnabledItems = (items) => items.filter((item) => item.enabled === true)
+const getEnabledItems = (items) =>
+  items.filter((item) => item.enabled === true);
 
 function FeatureGrid() {
-
   return (
     <div className="row flex flex-wrap">
-      {getEnabledItems(FEATURE_ITEMS_ALL).map((item)=><FeatureItem title={item.title} />)}
+      {getEnabledItems(FEATURE_ITEMS_ALL).map((item) => (
+        <FeatureItem title={item.title} />
+      ))}
     </div>
   );
 }
